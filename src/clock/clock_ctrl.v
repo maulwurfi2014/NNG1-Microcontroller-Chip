0c342e042e77b6ed4f4b349d869d384ecd485dcf
maulwurfi2014 <maulwurfi2014@gmx.de>
2026-09-02 23:54:43 +0200

    Delete src directory

--- src/clock/clock_ctrl.v
+++
@@ -1,5 +0,0 @@
-
-`timescale 1ns/1ps
-module clock_ctrl(input wire clk,rst_n,input wire[7:0]div_sel,output wire clk_en);
- reg[7:0]count;always@(posedge clk or negedge rst_n)if(!rst_n)count<=0;else if(count>=div_sel)count<=0;else count<=count+1;assign clk_en=(count==0);
-endmodule
